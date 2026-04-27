ad7db3b8f0aef1f7d94268f8a67a5a40bc47c07e
raianecardoso <raianecardoso.rede@gmail.com>
2026-04-27 18:17:43 -0300

    Ajusta readme e correção do exercício cadastro

--- exercicios/01_essencial/cadastro/_README.md
+++ exercicios/01_essencial/cadastro/_README.md
@@ -1,5 +1,5 @@
 ---
-Tags: Variáveis, Strings, Funções
+Tags: Variáveis, Strings
 Nível: Iniciante
 ---
 
@@ -15,23 +15,21 @@ Abra o arquivo main.py. Dentro dele, localize a função resposta.
 
 A função deverá receber três valores:
 
-- Nome (string)  
-- Idade (número inteiro)  
-- Altura (número decimal)  
+- Nome (string)
+- Idade (número inteiro)
+- Altura (número decimal)
 
 E deverá retornar uma string formatada com essas informações de forma organizada.
-
-Caso o exercício original peça entrada de dados com `input()`, considere que esses valores serão passados como parâmetros para a função resposta.
 
 Regras:
 
 - Utilizar os valores recebidos para montar a resposta
 - Organizar a saída em uma única string
-- Manter uma formatação clara e legível
+- Manter uma formatação clara e legível com o uso de |
 
 Exemplos:
 
-- `resposta("João", 25, 1.75)` → `"Nome: João | Idade: 25 | Altura: 1.75"`  
-- `resposta("Maria", 30, 1.60)` → `"Nome: Maria | Idade: 30 | Altura: 1.60"`  
+- `resposta("João", 25, 1.75)` → `"Nome: João | Idade: 25 | Altura: 1.75"`
+- `resposta("Maria", 30, 1.60)` → `"Nome: Maria | Idade: 30 | Altura: 1.6"`
 
 **Atenção:** utilize return, não print
--- exercicios/01_essencial/cadastro/tests/test_.py
+++ exercicios/01_essencial/cadastro/tests/test_.py
@@ -1,21 +1,20 @@
-from metro_milimetro.main import resposta
+from cadastro.main import resposta
 import inspect
 import pytest
 
 
 def test_not_none():
-    assert resposta(10) is not None, "Esperado valor diferente de 'None'"
+    assert resposta("João", 25, 1.75) is not None, "Esperado valor diferente de 'None'"
 
 
 def test_type():
-    assert type(resposta(13)) == int or type(resposta(3.2)) == float, "Esperado um inteiro ou float"
+    assert type(resposta("João", 25, 1.75)) == str, "Esperado uma string"
 
 
 def test_parameters():
-    assert len(inspect.getfullargspec(resposta).args) == 1, "Assinatura da função deverá receber um parâmetro"
+    assert len(inspect.getfullargspec(resposta).args) == 3, "Assinatura da função deverá receber três parâmetros"
 
 
 def test_options_resposta():
-    assert resposta(10) == 10000, f"Esperado valor 10000"
-    assert resposta(1.2) == 1200, f"Esperado valor 1200"
-    assert resposta(0.93) == 930, f"Esperado valor 930"
+    assert resposta("João", 25, 1.75) == 'Nome: João | Idade: 25 | Altura: 1.75', f"Esperado valor 'Nome: João | Idade: 25 | Altura: 1.75'"
+    assert resposta("Maria", 30, 1.60) == 'Nome: Maria | Idade: 30 | Altura: 1.6', f"Esperado valor 'Nome: Maria | Idade: 30 | Altura: 1.6'"
