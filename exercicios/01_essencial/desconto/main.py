1932ee6dc70ea6d769b78e7833014bde2819bcd7
Gabriel Braico Dornas <gabrielbdornas@gmail.com>
2026-07-10 14:20:08 +0000

    add solucao para desconto

--- exercicios/01_essencial/desconto/main.py
+++ exercicios/01_essencial/desconto/main.py
@@ -1,2 +1,13 @@
 def resposta(preço, desconto):
-    pass
+    desconto = preço * (desconto /100)
+    valor_final = preço - desconto
+    return (desconto, valor_final)
+
+
+#- Calcular o valor do desconto:
+#  - desconto = preço × (percentual / 100)
+#- Calcular o preço final:
+#  - preço final = preço - desconto
+#- Retornar os dois valores na ordem especificada
+
+desconto = resposta(152, 31)
