8774ab8e125ce5945cfca4580a4aeff2769f112f
Gabriel Braico Dornas <gabrielbdornas@gmail.com>
2026-07-10 14:59:25 +0000

    add exercicio contagem_palavra

--- exercicios/01_essencial/contagem_palavras/main.py
+++ exercicios/01_essencial/contagem_palavras/main.py
@@ -1,2 +1,8 @@
 def resposta(texto):
-    pass
+    texto = texto.lower()
+    lista = texto.split()
+    contador = {}
+    for palavra in lista:
+        contador[palavra] = contador.get(palavra, 0) + 1
+
+    return contador
