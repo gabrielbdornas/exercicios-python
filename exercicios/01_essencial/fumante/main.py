1f257a463fe3fbfc6fa7a57216bf431271dce2e1
Gabriel Braico Dornas <gabrielbdornas@gmail.com>
2026-07-24 14:15:39 +0000

    add exercicio fumante

--- exercicios/01_essencial/fumante/main.py
+++ exercicios/01_essencial/fumante/main.py
@@ -1,2 +1,11 @@
 def resposta(cigarros, anos):
-    pass
+    total_cigarros = cigarros * 365 * anos
+    total_minutos = total_cigarros * 10
+    dias_perdidos = total_minutos / 1440
+    return int(dias_perdidos)
+
+
+#- Considere que:
+#  - Cada cigarro reduz **10 minutos de vida**
+#  - 1 dia possui **1440 minutos**
+#- Calcule o total de minutos perdidos e converta para dias
