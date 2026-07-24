3643153888b0b2d0c1d72db7facb1495cd3763ce
Gabriel Braico Dornas <gabrielbdornas@gmail.com>
2026-07-24 14:20:22 +0000

    add exercicio carro_alugado

--- exercicios/01_essencial/carro_alugado/main.py
+++ exercicios/01_essencial/carro_alugado/main.py
@@ -1,2 +1,9 @@
 def resposta(km, dias):
-    pass
+    diarias = 60 * dias
+    km_rodado = 0.15 * km
+    return diarias + km_rodado
+
+
+# - O custo é composto por:
+#   - R$ 60,00 por dia
+#   - R$ 0,15 por quilômetro rodado
